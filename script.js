e075bd6d47b5d0189be7875cedeb77f0b17e3e83
Igor Molina <igordsmolina@gmail.com>
2020-11-12 12:46:21 -0300

    Postando projeto clicks

---
+++ script.js
@@ -0,0 +1,10 @@
+quantidade=0
+
+function count(){
+  document.getElementById('accum').innerHTML = quantidade += 1;
+    
+}
+
+function reset(){
+  document.getElementById('accum').innerHTML = quantidade = 0;
+}
